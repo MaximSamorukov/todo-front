e1fa2929799b6de496f4a4c58ce179ae0eb10c63
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-07-30 20:41:17 +0300

    added react-router-dom

--- index.jsx
+++ index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Users } from './src/HOC_Pages/Users.jsx';
 import './src/styles/style.css';
 import './src/styles/scss.scss';
@@ -7,7 +8,9 @@ import 'antd/dist/antd.css';
 
 
 const App = () => (
-  <Users />
+  <BrowserRouter>
+    <Users />
+  </BrowserRouter>
 )
 
 render( <App />, document.querySelector( '#root' ) );
